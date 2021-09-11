9dd67f41a15b659e03cb254d2e0170c531ef9dfd
Pedro Colombano <pedromecf@gmail.com>
2021-09-11 20:45:35 -0300

    Resource

--- backend/src/main/java/com/devsuperior/dslearn/entities/Offer.java
+++ backend/src/main/java/com/devsuperior/dslearn/entities/Offer.java
@@ -2,6 +2,8 @@ package com.devsuperior.dslearn.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Column;
@@ -11,6 +13,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -34,6 +37,9 @@ public class Offer implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "course_id")
 	private Course course;
+	
+	@OneToMany(mappedBy = "offer")
+	private List<Resource> resources = new ArrayList<>();
 	
 	public Offer() {
 		
@@ -85,6 +91,14 @@ public class Offer implements Serializable {
 
 	public void setCourse(Course course) {
 		this.course = course;
+	}
+	
+	public List<Resource> getResources() {
+		return resources;
+	}
+	
+	public void addResource(Resource resource) {
+		this.resources.add(resource);
 	}
 
 	@Override
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -21,5 +21,11 @@ INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Ficou 
 
 INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML 5', 'https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Switch-course-book-grey.svg/424px-Switch-course-book-grey.svg.png');
 
+
 INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', '2020-07-20T20:50:07Z', '2020-08-25T23:59:59Z', 1);
 INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', '2021-07-12T10:50:25Z', '2021-09-30T23:59:59Z', 1);
+
+
+INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Trilha HTML 5', 'Trilha principal do curso de HTML 5', 1, 'https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Fórum', 'Tire suas dúvidas', 2, 'https://www.teclasap.com.br/wp-content/uploads/2020/03/talk.png', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para alunos', 3, 'https://w7.pngwing.com/pngs/702/830/png-transparent-computer-icons-webcam-web-camera-electronics-share-icon-web-chat.png', 0, 1);
